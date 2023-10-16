51ad682fb7d7851973dd68c939409a32412b4afd
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 13:02:52 +0200

    Add project files.

---
+++ 1.B Summing 2 VALUES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.B_Summing_2_VALUES
+{
+    internal class Program
+    {
+        public static int SumTriple(int a, int b)
+        {
+            return a == b ? (a + b)*3 : a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumTriple(1, 2));
+            Console.WriteLine(SumTriple(3, 2));
+            Console.WriteLine(SumTriple(2, 2));
+        }
+        
+         
+    }
+}
